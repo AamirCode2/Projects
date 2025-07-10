190ebccebe1b6144605201e0e707b465324562d4
AamirCode2 <aamirlovesart@gmail.com>
2025-07-10 22:48:49 +1000

    Add files via upload

---
+++ Summer times.py
@@ -0,0 +1,9 @@
+temp = int(input("Enter the tempreture it is now: "))
+
+if temp > 20:
+    print("\nWear light clothing today \n")
+
+else:
+    print("\nWear thick clothing today \n")
+
+print("Have a good day!")
